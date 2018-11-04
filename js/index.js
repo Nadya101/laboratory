ac5c1740cd82deae146bb9c979d17b91dbd1ba66
nadya101 <angel.nadii36@gmail.com>
2018-11-04 02:53:55 +0000

    not ready

---
+++ js/index.js
@@ -0,0 +1,82 @@
+
+.transparent {
+    position: relative;
+    max-width: 100%;
+    padding: 60px 50px;
+    margin: 10px auto 0;
+    background-image: url(../img/blood-crop.jpg);
+    background-size: cover;
+    border-style: outset;
+}
+
+.form-inner {
+    position: relative;
+}
+
+.form-inner h3 {
+    position: relative;
+    margin-top: 0;
+    color: black;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    font-size: 26px;
+    text-transform: uppercase;
+}
+
+.form-inner label {
+    display: block;
+    padding-left: 15px;
+    font-family: 'Roboto', sans-serif;
+    color: black;
+    text-transform: uppercase;
+    font-size: 14px;
+}
+
+.form-inner input,
+#textarea {
+    display: block;
+    width: 100%;
+    padding: 15px 0;
+    margin: 10px 0 15px;
+    border-width: 0;
+    line-height: 40px;
+    border-radius: 20px;
+    color: black;
+    font-weight: bold;
+    background: rgba(255, 255, 255, .2);
+    font-family: 'Roboto', sans-serif;
+}
+
+.form-inner input[type="submit"] {
+    background: grey;
+    background-color: #e6c1ed;
+    box-shadow: 2px 3px 5px grey;
+}
+
+
+display:grid
+grid-template-columns: repeat (4, 1fr)
+
+.infoCtc {
+    background-image: url(../img/map.jpg);
+        background-size: contain;
+    text-transform: uppercase;
+    padding: 10px 0;
+}
+
+#phone, #adrs {
+    margin-left: 10px;
+    border-color: #ffffff;
+    border-left: 2px solid white;
+    border-radius: 5px;
+    width: 300px;
+   background-image: linear-gradient(to left, rgba(255,255,255,0), #ffffff);
+   font-family: 'Roboto', sans-serif; 
+}
+
+
+#adrs span,
+#phone span {
+    font-weight: bold;
+}
+*/
